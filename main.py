80de18647dcb5dfd0ea2e604942b1ad2eb6f03ef
Robert Furr <77758464+robtech21@users.noreply.github.com>
2021-06-02 15:36:12 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import duco_api
+from os import system
+from time import sleep
+from termcolor import colored
+from threading import Thread
+
+green       = 'green'
+red         = 'red'
+timerAmount = 0
+
+def figlet(text):
+    system('figlet -w 999 '+str(text))
+
+def clr():
+    system('clear')
+
+user    = input('user> ')
+passwd  = input('pass> ')
+
+clr()
+while True:
+    try:
+        api_connection = duco_api.api_actions()
+        api_connection.login(username=str(user), password=str(passwd))
+
+        price = duco_api.get_duco_price()
+        bal   = api_connection.balance()
+        value = float(bal) * float(price)
+        sleep(0.5)
+        clr()
+        figlet(text="'User:  '"+str(user))
+        figlet(text="'Duco:  '"+str(bal))
+        figlet(text="'Price: $'"+str(price))
+        figlet(text="'Money: $"+str(value)+"'")
+    except Exception as e:
+        #print(e)
+        pass
